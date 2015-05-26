a5fbd30aca184264e32dc98f5a1d4fb171050728
holleezzz <hollerupholleezzz@gmail.com>
2015-05-26 13:12:16 +0200

    Readme and WSDL

---
+++ WSDL/Console.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hahaha
+{
+    class Console1
+    {
+        static void Main(string[] args)
+        {
+			string choise = null;
+            WebService client = new WebService();
+			while(choise != "exit")	
+			{
+				Console.WriteLine("Chose to view file by typing its name or type \"exit\"");
+				string[] files = client.getAllFileNames();
+				foreach(string file in files)
+				{
+					Console.WriteLine(file);
+				}
+				choise = Console.ReadLine();
+				Console.WriteLine(client.getTxtFile(choise));
+			}
+        }
+    }
+}
---
+++ WSDL/Mats.bat
@@ -0,0 +1,8 @@
+﻿cd C:\Users\Hollerup\Documents\Visual Studio 2013\Projects\WebSite4\WSDL
+
+ "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\wsdl.exe" /out:Proxy.cs http://localhost:53269/WebService.asmx?WSDL
+ "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe" /t:library /out:Mats.dll Proxy.cs
+
+ "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe" /r:Mats.dll Console.cs
+ Console.exe
+ pause
